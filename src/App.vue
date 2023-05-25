b2bc99ae03e62a4cdb69c6a00849b303cf222d31
hulakk <hulakk@outlook.com>
2023-05-25 10:38:44 +0800

    Thu May 25 10:38:44 CST 2023

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,65 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+export default {
+  data: () => ({
+    gridData: [
+      { name: '优点云图', imgURL: "/images/advantagesCloud.png" },
+      { name: '缺点云图', imgURL: "/images/disadvantagesCloud.png" },
+      { name: '关键词云图', imgURL: "/images/wordcloud.png" },
+      { name: '评分图', imgURL: "/images/stars.png" }
+    ]
+  })
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="left">
+    <table v-for="item in gridData">
+      <tr>
+        <th>
+          {{ item.name }}
+        </th>
+        <th>
+          <img :src="item.imgURL" :alt="item.name">
+        </th>
+      </tr>
+    </table>
+    
+  
+      <!-- <img src="/images/advantagesCloud.png" alt="优点云图加载失败"/>
+      <br/>
+      <img src="/images/disadvantagesCloud.png" alt="缺点云图加载失败"/>
+      <br/>
+      <img src="/images/wordcloud.png" alt="关键词云图加载失败"/>
+      <br/>
+      <img src="/images/stars.png" alt="评分加载失败"/> -->
+  </div>
+  
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+body{
+  margin:0;
+  overflow: hidden;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1{
+    text-align: center;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
+div#v-model-test{
   text-align: center;
-  margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+div#v-model-test input{
+  font-size: xx-large;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+div#left{
+  background-color:  gray;
+  width: 100%;
+  left:0;
+  top:0;
+  position: absolute;
+  text-align: center;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+div#left img{
+  width: 50%;
 }
 </style>
