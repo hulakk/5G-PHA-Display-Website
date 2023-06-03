a3b2ce60897a497c52b4b32e01fcc00c75ff7dc6
hulakk <hulakk@outlook.com>
2023-06-03 22:40:26 +0800

    web基本完工

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,194 @@
 <script>
-
+import { ref } from 'vue'
+const activeName = ref('first')
 </script>
 <template>
   <el-config-provider namespace="ep">
 
     <BaseHeader />
 
-    <h1 id="dev_hint">······开发中······</h1>
+    <!-- <h1 id="dev_hint">······开发中······</h1> -->
+    <div style="text-align:center" >
+      <el-icon><Guide /></el-icon><br/>
+        <el-text class="page-title" type="default" tag="b">软件工程课程项目展示网站<br/></el-text>
+        <el-icon><Guide /></el-icon>
+    </div> 
 
-    <el-descriptions
-      class="margin-top"
-      title=""
-      column="2"
-      size="large"
-      border="true"
-      >
-    <!-- <template #extra>
-      <el-button type="primary">Operation</el-button>
-    </template> -->
-    <el-descriptions-item>
-      <template #label>
-        <div class="cell-item">
-          <el-icon :style="iconStyle">
-            <user />
-          </el-icon>
-          小组成员
-        </div>
-      </template>
-      <el-tag size="large">李嘉華</el-tag>
-      <el-tag size="large">周骏</el-tag>
-      <el-tag size="large">林乔瀚</el-tag>
-      <el-tag size="large">莫森华</el-tag>
-      <el-tag size="large">康文豪</el-tag>
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template #label>
-        <div class="cell-item">
-          <el-icon :style="iconStyle">
-            <iphone />
-          </el-icon>
-          学校
-        </div>
-      </template>
-      华南理工大学
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template #label>
-        <div class="cell-item">
-          <el-icon :style="iconStyle">
-            <location />
-          </el-icon>
-          题目
-        </div>
-      </template>
-      5G手机评论分析
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template #label>
-        <div class="cell-item">
-          <el-icon :style="iconStyle">
-            <tickets />
-          </el-icon>
-          指导老师
-        </div>
-      </template>
-      <el-tag size="large">1111</el-tag>
-    </el-descriptions-item>
-    </el-descriptions>
+      <el-icon><Guide /></el-icon>
+      <el-descriptions
+        class="margin-top"
+        title=""
+        column="2"
+        size="large"
+        border="true"
+        labelStyle="font-size:30px"
+        contentStyle="font-size:30px"
+        id="description-table"
+        >
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon>
+              <user />
+            </el-icon>
+            小组成员
+          </div>
+        </template>
+        <el-tag size="large"><el-text type="primary">李嘉華</el-text></el-tag>
+        <el-tag size="large"><el-text type="primary">周骏</el-text></el-tag>
+        <el-tag size="large"><el-text type="primary">林乔瀚</el-text></el-tag>
+        <el-tag size="large"><el-text type="primary">莫森华</el-text></el-tag>
+        <el-tag size="large"><el-text type="primary">康文豪</el-text></el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon>
+              <iphone />
+            </el-icon>
+            学校
+          </div>
+        </template>
+        <el-tooltip content="点击跳转到学校官网">
+        <el-link href="https://www.scut.edu.cn/" target="_blank">华南理工大学</el-link>
+        </el-tooltip>
+        <!-- <el-text type="default" tag="ins">华南理工大学</el-text> -->
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon>
+              <location />
+            </el-icon>
+            题目
+          </div>
+        </template>
+        <el-text type="default">5G手机评论分析</el-text>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon>
+              <tickets />
+            </el-icon>
+            指导老师
+          </div>
+        </template>
+        <el-tag size="large"><el-text type="primary">苏锦钿</el-text></el-tag>
+      </el-descriptions-item>
+      </el-descriptions>
+      <el-icon><Guide /></el-icon><br/>
+      <el-icon><Guide /></el-icon>
+    
 
     <div id="main">
+      <el-tooltip
+        content="回到顶部"
+      >
       <el-backtop :right="100" :bottom="100" />
+      </el-tooltip>
 
-      <el-carousel :interval="5000" arrow="always">
-        <el-carousel-item v-for="item in 4" :key="item">
-          <h3 text="2xl" justify="center">占位符{{ item }}</h3>
-        </el-carousel-item>
-      </el-carousel>
 
-      <el-tabs type="border-card">
-        <el-tab-pane name="first" disabled>
+
+      <el-tabs type="border-card" id="tab-pane" model-value="first">
+        <el-tab-pane name="zeroth" disabled>
           <template #label>
-            <h2>结 果 展 示</h2>
+            <!-- <h2>结 果 展 示</h2> -->
+            
+            <el-tooltip
+              class="box-item"
+              effect="dark"
+              content="请点击右边的标签以查看对应的图片"
+              placement="top-start"
+            >
+            <el-text class="tab-text" type="success" size="large">结果展示</el-text>
+          </el-tooltip>
           </template>
         </el-tab-pane>
         <el-tab-pane name="first">
           <template #label>
-            <h1>好评词云</h1>
+            <!-- <h1>好评词云</h1> -->
+            <el-text class="tab-text" type="primary" size="large">1.好评词云</el-text>
           </template>
-          <img v-loading="loading" src="/images/advantagesCloud.png" fit="cover"/>
+            <el-image v-loading="loading" src="/5G-PHA-Display-Website/images/advantagesCloud.png" fit="scale-down"/>
+           
         </el-tab-pane>
         <el-tab-pane name="second">
           <template #label>
-            <h1>差评词云</h1>
+            <!-- <h1>差评词云</h1> -->
+            <el-text class="tab-text" type="primary" size="large">2.差评词云</el-text>
           </template>
-          <img v-loading="loading" src="/images/disadvantagesCloud.png" fit="cover"/>
+          <el-image v-loading="loading" src="/5G-PHA-Display-Website/images/disadvantagesCloud.png" fit="scale-down"/>
         </el-tab-pane>
-        <el-tab-pane name="third">
-          <template #label>
-            <h1>关键词云图</h1>
+        <!-- <el-tab-pane name="third">
+          <template #label> -->
+            <!-- <h1>关键词云图</h1> -->
+            <!-- <el-text class="tab-text" type="primary" size="large">3.关键词词云</el-text>
           </template>
-          <img v-loading="loading" src="/images/wordcloud.png" fit="cover" />
-        </el-tab-pane>
+          <el-image v-loading="loading" src="/5G-PHA-Display-Website//images/wordcloud.png" fit="scale-down" />
+        </el-tab-pane> -->
         <el-tab-pane name="forth">
           <template #label>
-            <h1>星级统计图</h1>
+            <!-- <h1>星级统计图</h1> -->
+            <el-text class="tab-text" type="primary" size="large">3.星级统计图</el-text>
           </template>
-          <img v-loading="loading" src="/images/stars.png" fit="cover" />
+          <el-image v-loading="loading" src="/5G-PHA-Display-Website/images/stars.png" fit="scale-down" />
         </el-tab-pane>
       </el-tabs>
-    </div>
 
+      <el-icon><Guide /></el-icon><br/>
+      <el-icon><Guide /></el-icon>
+      <!-- <el-carousel :interval="5000" arrow="always" id="carousel">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 text="2xl" justify="center">carousel占位符{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel> -->
+    </div>
+    <!-- <el-empty description="完" /> -->
+    <br/><br/><br/><br/><br/><br/><br/><br/>
+
+    <el-container>
+      <el-footer style="text-align: center; font-size: 12px">
+        版权所有&copy本项目小组 2023
+      </el-footer>
+    </el-container>
     </el-config-provider>
 </template>
 
-<style>
-/* .demo-tabs > .el-tabs__content {
-  padding: 32px;
-  color: #6b778c;
-  font-size: 32px;
-  font-weight: 600;
+<style scoped>
+
+.tab-text{
+  font-size:28px;
 }
 
-.el-carousel__item h3 {
-  color: #475669;
-  opacity: 0.75;
-  line-height: 300px;
-  margin: 0;
-  text-align: center;
+#carousel{
+  background-color: aquamarine;
 }
 
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+#tab-pane{
+  background-color:gray;
+  position: relative;
+  width: 70%;
+  left:15%;
 }
 
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
-} */
+#description-table *{
+  font-size: 30px;
+  font-family: 'SimSun';
+
+}
+
+.page-title{
+  font-size: 40px !important;
+  /* text-align: center !important; //不起作用 */
+  font-family: 'SimSun';
+}
+
 div#main{
-  width: 100%;
-  left:0;
+  /* width: 100%; */
+  /* left:0; */
   /* top:0; */
-  position: absolute;
+  /* position: absolute; */
   text-align: center;
   font-family: "STXingkai","Microsoft YaHei";
 }
--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -4,24 +4,42 @@ import { toggleDark } from "~/composables";
 
 <template>
   <el-menu class="el-menu-demo" mode="horizontal">
-    <el-menu-item index="1" disabled>5G手机评价分析</el-menu-item>
+    <el-menu-item index="1" disabled>
+      <el-text type="success">5G手机评价分析</el-text>
+    </el-menu-item>
     <el-sub-menu index="2">
       <template #title>分工</template>
       <el-sub-menu index="2-1">
-        <template #title>爬虫小分组</template>
-        <el-menu-item index="2-1-1">林乔瀚</el-menu-item>
+        <template #title>
+          <el-text class="division-of-labor">爬虫小分组</el-text>
+        </template>
+        <el-menu-item index="2-1-1">
+          <el-text class="division-of-labor">林乔瀚</el-text>
+        </el-menu-item>
       </el-sub-menu>
 
       <el-sub-menu index="2-2">
-        <template #title>建模小分组</template>
-        <el-menu-item index="2-2-1">李嘉華</el-menu-item>
-        <el-menu-item index="2-2-2">周骏</el-menu-item>
-        <el-menu-item index="2-2-3">莫森华</el-menu-item>
+        <template #title>
+          <el-text class="division-of-labor">建模小分组</el-text>
+        </template>
+        <el-menu-item index="2-2-1">
+          <el-text class="division-of-labor">李嘉華</el-text>
+        </el-menu-item>
+        <el-menu-item index="2-2-2">
+          <el-text class="division-of-labor">周骏</el-text>
+        </el-menu-item>
+        <el-menu-item index="2-2-3">
+          <el-text class="division-of-labor">莫森华</el-text>
+        </el-menu-item>
       </el-sub-menu>
 
       <el-sub-menu index="2-3">
-        <template #title>网站搭建小分组</template>
-        <el-menu-item index="2-3-1">康文豪</el-menu-item>
+        <template #title>
+          <el-text class="division-of-labor">网站搭建小分组</el-text>
+        </template>
+        <el-menu-item index="2-3-1">
+          <el-text class="division-of-labor">康文豪</el-text>
+        </el-menu-item>
       </el-sub-menu>
       
     </el-sub-menu>
@@ -31,9 +49,13 @@ import { toggleDark } from "~/composables";
     trigger="click"
   >
     <template #reference>
-      <el-menu-item index="3" ref="buttonRef">Info</el-menu-item>
+      <el-menu-item index="3" ref="buttonRef">
+        <el-text type="default">项目介绍</el-text>
+      </el-menu-item>
     </template>
-      待补充
+    <el-text class="info-text" type="info">进入5G时代，各大手机厂商纷纷推出5G手机，为了真实评估各主流的5G手机的用户口碑，可以登陆到主流电商网站，根据用户评价，识别出用户反馈的优劣势以及关注倾向度，从而客观评价5G手机的真实反馈。</el-text>
+    <p></p>
+    <el-text class="info-text" type="info">登陆主流电商网站（如京东、天猫等），锁定主流的5G手机（如VIVO X90、MI 13、IPHONE14等），分别获取各款手机完整的商品评价，通过对文本解析和分词处理，并剔除异常数据，汇总后得出各款手机的优点和缺点，为消费者提供选型参考。</el-text>
   </el-popover>
     
     <!-- <el-menu-item index="4">Orders</el-menu-item> -->
